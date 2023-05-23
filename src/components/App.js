371e516f5e6833b311be3917d890aa9c6279a0f7
Andrey Roditelev <rodandr13@gmail.com>
2023-05-23 20:32:34 +0300

    feat: add Main component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import logo from '../images/header/logo.svg'
 import '../index.css'
 
 import Header from './Header'
+import Main from "./Main";
 
 function App() {
   return (
@@ -9,27 +10,8 @@ function App() {
       <div className="root">
         <div className="page">
         <Header logo={logo} />
-          <main className="main">
-            <section className="profile">
-              <div className="profile__container">
-                <a href="src#" className="profile__avatar-link"><img src="<%=require('./images/profile/avatar.jpg')%>"
-                                                                     alt="Аватар: Жак-Ив Кусто" className="profile__avatar" /></a>
-                <div className="profile__info">
-                  <div className="profile__text-container">
-                    <h1 className="profile__header">Жак-Ив Кусто</h1>
-                    <p className="profile__subheader">Исследователь океана</p>
-                  </div>
-                  <button type="button" className="profile__edit-button" aria-label="Изменить профиль"></button>
-                </div>
-              </div>
-              <button type="button" className="profile__add-button" aria-label="Добавить место"></button>
-            </section>
-            <section className="elements">
-              <ul className="elements__list">
+          <Main />
 
-              </ul>
-            </section>
-          </main>
           <footer className="footer">
             <p className="footer__copyright">© 2023 Mesto Russia</p>
           </footer>
---
+++ src/components/Main.js
@@ -0,0 +1,29 @@
+function Main(props) {
+    <main className="main">
+        <section className="profile">
+            <div className="profile__container">
+                <a href="src#" className="profile__avatar-link">
+                    <img src="<%=require('./images/profile/avatar.jpg')%>"
+                         alt="Аватар: Жак-Ив Кусто"
+                         className="profile__avatar"
+                    />
+                </a>
+                <div className="profile__info">
+                    <div className="profile__text-container">
+                        <h1 className="profile__header">Жак-Ив Кусто</h1>
+                        <p className="profile__subheader">Исследователь океана</p>
+                    </div>
+                    <button type="button" className="profile__edit-button" aria-label="Изменить профиль"></button>
+                </div>
+            </div>
+            <button type="button" className="profile__add-button" aria-label="Добавить место"></button>
+        </section>
+        <section className="elements">
+            <ul className="elements__list">
+
+            </ul>
+        </section>
+    </main>
+}
+
+export default Main;
