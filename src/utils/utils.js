2946bb211cddc7047158abacdf235aecc47f4ec2
Andrey Roditelev <rodandr13@gmail.com>
2023-05-24 18:13:32 +0300

    fix: edit reamde

--- src/utils/utils.js
+++
@@ -1,8 +0,0 @@
-export const validationOptions = {
-  formSelector: '.popup__form',
-  inputSelector: '.form__input',
-  submitButtonSelector: '.form__button',
-  inactiveButtonClass: 'form__button_inactive',
-  inputErrorClass: 'form__input_type_error',
-  errorClass: 'form__input-error_active'
-}
